d62036a98c1a4a65080654c412b7cbaf56129cd4
BasselOjjeh <bassel_ojjeh@asl.org>
2024-03-15 10:29:19 +0000

    Carousel draft 1

--- main.js
+++ main.js
@@ -1,50 +1,60 @@
 // change navbar styles on scroll
-
 window.addEventListener('scroll', () => {
-    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0)
-})
-
-
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0);
+});
 
 // show/hide faq answer 
-
 const faqs = document.querySelectorAll('.faq');
-
 faqs.forEach(faq => {
     faq.addEventListener('click', () => {
         faq.classList.toggle('open');
 
         // change icon
         const icon = faq.querySelector('.faq__icon i');
-        if(icon.className === 'uil uil-plus') {
+        if (icon.className === 'uil uil-plus') {
             icon.className = "uil uil-minus";
         } else {
             icon.className = "uil uil-plus";
-
         }
-    })
-})
-
-
+    });
+});
 
 // show/hide nav menu
 const menu = document.querySelector(".nav__menu");
 const menuBtn = document.querySelector("#open-menu-btn");
 const closeBtn = document.querySelector("#close-menu-btn");
 
-
 menuBtn.addEventListener('click', () => {
     menu.style.display = "flex";
     closeBtn.style.display = "inline-block";
     menuBtn.style.display = "none";
-})
-
+});
 
 // close nav menu 
-const closeNav = () => {
+closeBtn.addEventListener('click', closeNav);
+
+function closeNav() {
     menu.style.display = "none";
     closeBtn.style.display = "none";
     menuBtn.style.display = "inline-block";
 }
 
-closeBtn.addEventListener('click', closeNav)
+// Carousel JavaScript code
+
+let slideIndex = 0;
+showSlides(slideIndex);
+
+function moveSlide(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName("carousel__item");
+    if (n >= slides.length) slideIndex = 0;
+    if (n < 0) slideIndex = slides.length - 1;
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slides[slideIndex].style.display = "block";
+}
+
